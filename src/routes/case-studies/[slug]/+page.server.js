7293aea60207211ebb3eadecf319e8ad56a155d3
Nate Green <heynategreen@gmail.com>
2024-01-28 16:05:39 -0500

    add drafts capability to case study pages

--- src/routes/case-studies/[slug]/+page.server.js
+++ src/routes/case-studies/[slug]/+page.server.js
@@ -1,8 +1,18 @@
-import { gql, request } from 'graphql-request';
+import { GraphQLClient } from 'graphql-request';
+import { dev } from '$app/environment';
+import { HYGRAPH_DEV_AUTH_TOKEN } from '$env/static/private';
+import { gql } from 'graphql-request';
 
 export async function load({ params }) {
-  const caseStudies = await request(
-    "https://api-us-east-1-shared-usea1-02.hygraph.com/v2/clnhty2q88bul01uhf5m5dqrj/master",
+  const hygraph = new GraphQLClient(
+    'https://api-us-east-1-shared-usea1-02.hygraph.com/v2/clnhty2q88bul01uhf5m5dqrj/master',
+    {
+      headers: dev ? {
+        'Authorization': `Bearer ${HYGRAPH_DEV_AUTH_TOKEN}`,
+      } : {},
+    }
+  )
+  const caseStudies = await hygraph.request(
     gql`{
       caseStudy(where: {slug: "${params.slug}"}) {
         id
